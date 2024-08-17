0d282fa6981fbc22bc3d770c4fb49dce51b024d3
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-08-17 03:59:31 +0300

    build the registration modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Nunito } from "next/font/google";
 
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal isOpen />
         <Navbar />
         {children}
         </body>
